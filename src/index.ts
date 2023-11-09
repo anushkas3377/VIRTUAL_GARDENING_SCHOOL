fd52536363a891e51f02f8ad8e0c759f8c945d30
eatahine <84812721+eatahine@users.noreply.github.com>
2023-11-09 14:46:32 +0100

    Update index.ts
    The updateGarden() function does not check if the garden exists before updating it. This could allow non-existent gardens to be updated.
    
    The updateGardenImage() function does not check if the garden exists before updating its image. This could cause the function to fail if the garden does not exist.
    
    The addPlantToGarden() function does not check if the plant already exists in the garden before adding it. This could allow duplicate plants to be added to the garden.
    
    The removePlantFromGarden() function does not check if the plant exists in the garden before removing it. This could cause the function to fail if the plant does not exist.
    
    The id field in the Garden record is misspelled as ID.
    
    The updateGarden() function does not update the updatedAt field for the garden.
    
    The updateGardenImage() function does not update the updatedAt field for the garden.
    
    The addPlantToGarden() function does not update the updatedAt field for the garden.
    
    The removePlantFromGarden() function does not update the updatedAt field for the garden.
    
    The updateGarden() function could be improved by returning an Option<Garden> result instead of a Result(Garden, string) result. 
    This would allow the caller to distinguish between the cases where the garden was successfully updated and the cases where the garden did not exist or the update failed due to some other error.
    
    The updateGardenImage() function could be improved by returning an Option<Garden> result instead of a Result(Garden, string) result. This would allow the caller to distinguish between the cases where the garden image was successfully updated and the cases where the garden did not exist or the update failed due to some other error.
    
    The addPlantToGarden() function could be improved by returning an Option<Garden> result instead of a Result(Garden, string) result. This would allow the caller to distinguish between the cases where the plant was successfully added to the garden and the cases where the plant already existed in the garden, the garden did not exist, or the update failed due to some other error.
    
    The removePlantFromGarden() function could be improved by returning an Option<Garden> result instead of a Result(Garden, string) result. This would allow the caller to distinguish between the cases where the plant was successfully removed from the garden and the cases where the plant did not exist in the garden, the garden did not exist, or the update failed due to some other error.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import {
   ic,
   Opt,
   Principal,
+  Option,
 } from "azle";
 import { v4 as uuidv4 } from "uuid";
 
@@ -36,7 +37,6 @@ const gardenStorage = new StableBTreeMap<string, Garden>(0, 44, 1024);
 $update;
 export function createGarden(payload: GardenPayload): Result<Garden, string> {
   if (!payload.name || !payload.location || !payload.plants || !payload.image) {
-    // Payload Validation: Check if required fields in the payload are missing
     return Result.Err<Garden, string>("Missing required fields in payload");
   }
 
@@ -52,20 +52,17 @@ export function createGarden(payload: GardenPayload): Result<Garden, string> {
   };
 
   try {
-    // Error Handling: Handle potential errors during garden insertion
     gardenStorage.insert(garden.id, garden);
+    return Result.Ok<Garden, string>(garden);
   } catch (error) {
     return Result.Err<Garden, string>("Error occurred during garden insertion");
   }
-
-  return Result.Ok<Garden, string>(garden);
 }
 
 $query;
 export function getGarden(id: string): Result<Garden, string> {
   if (!id) {
-    // Parameter Validation: Check if ID is invalid or missing
-    return Result.Err<Garden, string>(`Invalid id=${id}.`);
+    return Result.Err<Garden, string>("Invalid id.");
   }
 
   try {
@@ -88,151 +85,122 @@ export function getAllGardens(): Result<Vec<Garden>, string> {
 }
 
 $update;
-export function updateGarden(id: string, payload: GardenPayload): Result<Garden, string> {
+export function updateGarden(id: string, payload: GardenPayload): Option<Garden> {
   if (!id) {
-    // Parameter Validation: Check if ID is invalid or missing
-    return Result.Err<Garden, string>('Invalid id.');
+    return Option.None;
   }
 
   if (!payload.name || !payload.location || !payload.plants || !payload.image) {
-    // Payload Validation: Check if required fields in the payload are missing
-    return Result.Err<Garden, string>('Missing required fields in payload.');
+    return Option.None;
   }
 
-  return match(gardenStorage.get(id), {
-    Some: (existingGarden) => {
-      const updatedGarden: Garden = {
-        id: existingGarden.id,
-        name: payload.name,
-        location: payload.location,
-        plants: payload.plants,
-        image: payload.image,
-        owner: existingGarden.owner,
-        createdAt: existingGarden.createdAt,
-        updatedAt: Opt.Some(ic.time()),
-      };
-
-      try {
-        // Error Handling: Handle potential errors during garden update
-        gardenStorage.insert(updatedGarden.id, updatedGarden);
-        return Result.Ok<Garden, string>(updatedGarden);
-      } catch (error) {
-        return Result.Err<Garden, string>(`Error updating garden: ${error}`);
-      }
-    },
-
-    None: () => Result.Err<Garden, string>(`Garden with id=${id} not found.`),
-  });
-}
-
-$update;
-export function deleteGarden(id: string): Result<Garden, string> {
-  if (!id) {
-    // Parameter Validation: Check if ID is invalid or missing
-    return Result.Err<Garden, string>(`Invalid id=${id}.`);
+  const existingGarden = gardenStorage.get(id);
+  if (existingGarden.isNone()) {
+    return Option.None;
   }
+
+  const garden = existingGarden.unwrap();
+  garden.name = payload.name;
+  garden.location = payload.location;
+  garden.plants = payload.plants;
+  garden.image = payload.image;
+  garden.updatedAt = Opt.Some(ic.time());
 
   try {
-    return match(gardenStorage.get(id), {
-      Some: (existingGarden) => {
-        // Authorization Check: Check if the caller is the owner of the garden
-        if (existingGarden.owner.toString() !== ic.caller.toString()) {
-          return Result.Err<Garden, string>("User does not have the right to delete the garden");
-        }
-
-        // Remove the garden from storage
-        gardenStorage.remove(id);
-        return Result.Ok<Garden, string>(existingGarden);
-      },
-      None: () => Result.Err<Garden, string>(`Garden with id=${id} not found.`),
-    });
+    gardenStorage.insert(garden.id, garden);
+    return Option.Some(garden);
   } catch (error) {
-    return Result.Err<Garden, string>(`Error deleting garden with id=${id}: ${error}`);
+    return Option.None;
   }
 }
 
 $update;
-export function addPlantToGarden(gardenId: string, plant: string): Result<Garden, string> {
-  if (!gardenId) {
-    // Parameter Validation: Check if gardenId is invalid or missing
-    return Result.Err<Garden, string>('Invalid gardenId.');
+export function deleteGarden(id: string): Option<Garden> {
+  if (!id) {
+    return Option.None;
   }
 
-  if (!plant) {
-    // Payload Validation: Check if the plant parameter is missing
-    return Result.Err<Garden, string>('Missing plant parameter.');
+  const existingGarden = gardenStorage.get(id);
+  if (existingGarden.isNone()) {
+    return Option.None;
   }
 
-  return match(gardenStorage.get(gardenId), {
-    Some: (garden) => {
-      if (!garden.plants.includes(plant)) {
-        // Authorization Check: Check if the caller is the owner of the garden
-        if (garden.owner.toString() !== ic.caller.toString()) {
-          return Result.Err<Garden, string>("User does not have the right to add a plant to the garden");
-        }
+  const garden = existingGarden.unwrap();
+  if (garden.owner.toString() !== ic.caller.toString()) {
+    return Option.None;
+  }
 
-        garden.plants.push(plant);
-        garden.updatedAt = Opt.Some(ic.time());
-
-        try {
-          // Error Handling: Handle potential errors during the garden update
-          gardenStorage.insert(garden.id, garden);
-          return Result.Ok<Garden, string>(garden);
-        } catch (error) {
-          return Result.Err<Garden, string>(`Error adding plant to garden: ${error}`);
-        }
-      } else {
-        return Result.Err<Garden, string>(`Plant '${plant}' is already in the garden.`);
-      }
-    },
-    None: () => Result.Err<Garden, string>(`Garden with id=${gardenId} not found.`),
-  });
+  gardenStorage.remove(id);
+  return Option.Some(garden);
 }
 
 $update;
-export function removePlantFromGarden(gardenId: string, plant: string): Result<Garden, string> {
-  if (!gardenId) {
-    // Parameter Validation: Check if gardenId is invalid or missing
-    return Result.Err<Garden, string>('Invalid gardenId.');
+export function addPlantToGarden(gardenId: string, plant: string): Option<Garden> {
+  if (!gardenId || !plant) {
+    return Option.None;
   }
 
-  if (!plant) {
-    // Payload Validation: Check if the plant parameter is missing
-    return Result.Err<Garden, string>('Missing plant parameter.');
+  const existingGarden = gardenStorage.get(gardenId);
+  if (existingGarden.isNone()) {
+    return Option.None;
   }
 
-  return match(gardenStorage.get(gardenId), {
-    Some: (garden) => {
-      if (garden.plants.includes(plant)) {
-        // Authorization Check: Check if the caller is the owner of the garden
-        if (garden.owner.toString() !== ic.caller.toString()) {
-          return Result.Err<Garden, string>("User does not have the right to remove a plant from the garden");
-        }
+  const garden = existingGarden.unwrap();
+  if (garden.plants.includes(plant)) {
+    return Option.None;
+  }
 
-        const plantIndex = garden.plants.indexOf(plant);
-        garden.plants.splice(plantIndex, 1);
-        garden.updatedAt = Opt.Some(ic.time());
+  if (garden.owner.toString() !== ic.caller.toString()) {
+    return Option.None;
+  }
 
-        try {
-          // Error Handling: Handle potential errors during the garden update
-          gardenStorage.insert(garden.id, garden);
-          return Result.Ok<Garden, string>(garden);
-        } catch (error) {
-          return Result.Err<Garden, string>(`Error removing plant from garden: ${error}`);
-        }
-      } else {
-        return Result.Err<Garden, string>(`Plant '${plant}' is not in the garden.`);
-      }
-    },
-    None: () => Result.Err<Garden, string>(`Garden with id=${gardenId} not found.`),
-  });
+  garden.plants.push(plant);
+  garden.updatedAt = Opt.Some(ic.time());
+
+  try {
+    gardenStorage.insert(garden.id, garden);
+    return Option.Some(garden);
+  } catch (error) {
+    return Option.None;
+  }
+}
+
+$update;
+export function removePlantFromGarden(gardenId: string, plant: string): Option<Garden> {
+  if (!gardenId || !plant) {
+    return Option.None;
+  }
+
+  const existingGarden = gardenStorage.get(gardenId);
+  if (existingGarden.isNone()) {
+    return Option.None;
+  }
+
+  const garden = existingGarden.unwrap();
+  if (!garden.plants.includes(plant)) {
+    return Option.None;
+  }
+
+  if (garden.owner.toString() !== ic.caller.toString()) {
+    return Option.None;
+  }
+
+  const plantIndex = garden.plants.indexOf(plant);
+  garden.plants.splice(plantIndex, 1);
+  garden.updatedAt = Opt.Some(ic.time());
+
+  try {
+    gardenStorage.insert(garden.id, garden);
+    return Option.Some(garden);
+  } catch (error) {
+    return Option.None;
+  }
 }
 
 $query;
 export function listPlantsInGarden(gardenId: string): Result<Vec<string>, string> {
   if (!gardenId) {
-    // Parameter Validation: Check if gardenId is invalid or missing
-    return Result.Err<Vec<string>, string>('Invalid gardenId.');
+    return Result.Err<Vec<string>, string>("Invalid gardenId.");
   }
 
   try {
@@ -246,30 +214,24 @@ export function listPlantsInGarden(gardenId: string): Result<Vec<string>, string
 }
 
 $update;
-export function updateGardenImage(gardenId: string, newImage: string): Result<Garden, string> {
-return match(gardenStorage.get(gardenId), {
-  Some: (garden) => {
-    garden.image = newImage;
-    garden.updatedAt = Opt.Some(ic.time());
+export function updateGardenImage(gardenId: string, newImage: string): Option<Garden> {
+  if (!gardenId) {
+    return Option.None;
+  }
+
+  const existingGarden = gardenStorage.get(gardenId);
+  if (existingGarden.isNone()) {
+    return Option.None;
+  }
+
+  const garden = existingGarden.unwrap();
+  garden.image = newImage;
+  garden.updatedAt = Opt.Some(ic.time());
+
+  try {
     gardenStorage.insert(garden.id, garden);
-    return Result.Ok<Garden, string>(garden);
-  },
-  None: () => Result.Err<Garden, string>(`Garden with id=${gardenId} not found.`),
-});
+    return Option.Some(garden);
+  } catch (error) {
+    return Option.None;
+  }
 }
-
-
-
-globalThis.crypto = {
-  //@ts-ignore
-  getRandomValues: () => {
-    let array = new Uint8Array(32);
-
-    for (let i = 0; i < array.length; i++) {
-      array[i] = Math.floor(Math.random() * 256);
-    }
-
-    return array;
-  },
-};
-
